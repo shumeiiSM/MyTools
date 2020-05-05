2b75eaf430a49e1009fd266368e5cf1751dde484
shumeii <shumei023@gmail.com>
2020-05-05 20:36:49 +0800

    Fragment Tools

--- app/src/main/java/com/example/mytools/AllTools.java
+++ app/src/main/java/com/example/mytools/AllTools.java
@@ -1,29 +1,40 @@
 package com.example.mytools;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class AllTools extends AppCompatActivity {
+import java.util.List;
+
+public class AllTools extends Fragment {
 
     ImageView ivBack;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_tools);
+    private RecyclerView mRecyclerView;
+    private List<AllToolDisplay> toolList;
 
-        ivBack = findViewById(R.id.ivBack);
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_all_tools, container, false);
+
+        ivBack = view.findViewById(R.id.ivBack);
 
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentNewAct = new Intent(getBaseContext(), MainActivity.class);
+                Intent intentNewAct = new Intent(getContext(), MainActivity.class);
                 startActivity(intentNewAct);
             }
         });
+
+        return view;
     }
 }
